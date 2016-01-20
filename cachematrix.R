da2e983969ed24c68ae5ed7954a9ab9a466f1ee8
Girum Bizuayehu <girum.bizuayehu@kimetrica.com>
2016-01-20 14:10:25 +0300

    First submission of programming assignment II

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## As indicated, matrix inversion could be computationally expensive
+## Using the <<- operator it is possible to store the inversions of a matrix for later use
 
-## Write a short comment describing this function
+
+
+## makeCacheMatrix creates a matrix and store it in the environment
+## if the value is not null, the assumption being the matrix is relevant 
+## for later use and should be cached accordingly
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   
+   set <- function(m){
+          if(is.null(m))
+               x <<-m
+   }
+   get <- function() x
+   
+   
+   list (set=set,get=get)
+  
 }
 
 
-## Write a short comment describing this function
+## solve method can be used for computing the inverse of a square matrix
+## saving the result of the inverse of a matrix can therefore be useful
+## cacheSolve retrives the values of a saved result of an inverse of a matrix
+
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    cachedMatrix <- x$get()
+       if(!is.null(cachedMatrix))
+          return  (solve(cachedMatrix))
+    
+    NULL
 }
